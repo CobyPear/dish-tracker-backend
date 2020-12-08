0be55489b643e94fc66f7f7c9e929d3b1fc63e67
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-12-08 13:57:16 -0600

    added express.static to try to resolve heroku 503

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
+import path from 'path'
+const __dirname = path.resolve()
+
 dotenv.config()
 
 const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
 import restaurantRoutes from './routes/restaurantRoutes.js'
 import menuRoutes from './routes/menuRoutes.js'
