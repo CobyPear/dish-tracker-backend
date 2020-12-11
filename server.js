cab8a69ea635df5663562b4372661a6a39c07692
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-12-11 10:19:15 -0600

    debugging heroku

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
+import timeout from 'connect-timeout'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 8080
 
+app.use(timeout('10s'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
