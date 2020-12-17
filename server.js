8ea5f7aae67138397352e5f64fb88b96638cf454
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-12-17 10:10:52 -0600

    adding CORS

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 
+const corsOptions = {}
+
+const handleCors = (options) => {
+    return (req, res, next) => {
+        res.set('Access-Control-Allow-Origin', null)
+        if (isPreflight(req)) {
+            res.set('Access-Control-Allow-Methods', 'GET')
+            res.set('Access-Control-Max-Age', '10000')
+            res.status(204).end()
+            return
+        } else {
+            res.set('Access-Control-Expose-Headers', 'X-Powered-By')
+        }
+        next()
+    }
+}
+
 const PORT = process.env.PORT || 8080
 
 app.use(express.urlencoded({ extended: true }));
@@ -9,6 +26,8 @@ app.use(express.json())
 
 const restaurantRoutes = require('./routes/restaurantRoutes.js')
 const menuRoutes = require('./routes/menuRoutes.js')
+
+app.use(handleCors(corsOptions))
 
 app.use('/api/restaurants', restaurantRoutes)
 app.use('/api/menu', menuRoutes)
